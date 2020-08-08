8405b3effc3a926033b6e9342619e082f8bb7d0c
payney1701 <robbiepayne1701@gmail.com>
2020-08-08 13:43:23 +0100

    play again button and random number generator

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Game Functions:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI Elements
@@ -25,16 +25,23 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again'){
+    window.location.reload();
+  }
+});
+
 // Listen for guess
 guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
   
-  //Validate
+  // Validate
   if(isNaN(guess) === NaN || guess < min || guess > max){
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   }
 
-  //Check if won
+  // Check if won
   if(guess === winningNum){
     // Game over - won
     gameOver(true, `${winningNum} is correct, YOU WIN!`);
@@ -44,7 +51,7 @@ guessBtn.addEventListener('click', function(){
     guessesLeft -= 1;
 
     if(guessesLeft === 0){
-      //Game over - lost
+      // Game over - lost
       gameOver(false, `GAME OVER, you lost. The correct number was ${winningNum}`);
     } else {
       // Game continues - answer wrong
@@ -52,7 +59,7 @@ guessBtn.addEventListener('click', function(){
       // Change border color
       guessInput.style.borderColor = 'red';
 
-      //Clear input
+      // Clear input
       guessInput.value = '';
 
       // Tell user its the wrong number
@@ -74,6 +81,15 @@ function gameOver(won, msg){
   message.style.color = color;
   // Set message
   setMessage(msg);
+
+  // Play again
+  guessBtn.value = 'Play Again?'
+  guessBtn.className += 'play-again';
+}
+
+// Get winning number
+function getRandomNum(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // Set message
